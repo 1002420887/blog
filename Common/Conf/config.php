c601d687dbfb19e4da195cdda0f804e391d1da4f
cyf180 <654596641@qq.com>
2017-05-26 14:11:52 +0800

    飞哥代码注释

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -7,7 +7,7 @@ return array(
 	'DB_HOST'               =>  '127.0.0.1', // 主机地址
     'DB_NAME'               =>  'sime',     // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '228478',         // 密码
+    'DB_PWD'                =>  'root',         // 密码
     'DB_PREFIX'             =>  'sime_',    // 数据库前缀
     
 
@@ -16,7 +16,7 @@ return array(
     'URL_MODEL'             =>2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
     
-
+    // 'TMPL_TEMPLATE_SUFFIX'  =>  '.tpl',     // 模板后缀 
     'URL_HTML_SUFFIX'       =>  'html',  // URL伪静态后缀
     'DEFAULT_FILTER'        =>  'strip_tags,htmlspecialchars', // 默认参数过滤方法 用于I函数... 
 
--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -28,6 +28,31 @@ class IndexController extends Controller{
 		$this->display();
 	}
 
+	public function index1(){
+		$uid = session(C('WEB_SESSION'))['uid'];
+
+		$data  = D('Blog');
+		$cData = D('User_cla');
+
+		$cid   = I('cid');
+
+		//数据列表
+		// $where = 'del = 0';
+		$where .= $uid  ? ' and b.uid = '.$uid : '';
+		$where .= $cid  ? ' and FIND_IN_SET('.$cid.',b.pid)' : '' ;
+		
+		$field = 'b.*,c.name';
+		$order = 'b.id desc';
+
+		$list  = $data->sel($where,15,$field,$order);
+		
+		$this->assign('cid',$cid);
+		$this->assign('uid',$uid);
+		$this->assign('list',$list['list']);
+		$this->assign('page',$list['page']);
+		$this->display();		
+	}
+
 	// //内容显示页
 	// function cont(){
 	// 	$data = M('blog');
